5ef34c9c8a5a5c6cf343acd2d403e9bf40f98393
Ariel <arielox.ag@gmail.com>
2022-01-27 20:54:39 -0300

    inicial

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
 app.get('/', (req, res) => {
-  res.send('<h1>File Upload Starter</h1>');
+   res.send('<h1>File Upload Starter</h1>');
 });
 
 // middleware
@@ -22,15 +22,15 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
+   try {
+      await connectDB(process.env.MONGO_URI);
 
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+      app.listen(port, () =>
+         console.log(`Server is listening on port ${port}...`)
+      );
+   } catch (error) {
+      console.log(error);
+   }
 };
 
 start();
